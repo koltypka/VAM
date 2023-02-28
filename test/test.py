d3a1997de7b044581c40bc05eb383c7faebcb0b6
Всеволод Колтыпин <koltypin@e-krit.ru>
2023-02-28 22:36:28 +0300

    начало юнит тестов

--- test/test.py
+++ test/test.py
@@ -2,19 +2,9 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
-
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # Проверим, что s.split не работает, если разделитель - не строка
-      with self.assertRaises(TypeError):
-          s.split(2)
+  def test_test(self):
+      self.assertTrue([1,2,3] == [1,2,3])
+      self.assertFalse([1,2,3] == dict('test'))
 
 if __name__ == '__main__':
     unittest.main()
